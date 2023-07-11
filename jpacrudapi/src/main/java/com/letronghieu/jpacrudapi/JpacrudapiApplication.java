caef4a7835c391a69e0885fe0da0a38474da033d
Le Trong Hieu <letronghieu3004.utm@gmail.com>
2023-07-11 14:23:13 +0700

    demo jpa spring boot 1.0

---
+++ jpacrudapi/src/main/java/com/letronghieu/jpacrudapi/JpacrudapiApplication.java
@@ -0,0 +1,13 @@
+package com.letronghieu.jpacrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpacrudapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpacrudapiApplication.class, args);
+	}
+
+}
---
+++ jpacrudapi/src/test/java/com/letronghieu/jpacrudapi/JpacrudapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letronghieu.jpacrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpacrudapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
